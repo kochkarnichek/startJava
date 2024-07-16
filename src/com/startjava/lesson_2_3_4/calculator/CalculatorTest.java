667be6e1feaee5cc66765b04ff8cc1e9a98bdf82
kochkarnichek <lyubansha2708@gmail.com>
2024-07-16 23:38:18 +0200

    implement HW for Lesson_4 p.3

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,27 +1,37 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
-        String choice;
+        String choice = "yes";
         do {
-            System.out.println("Введите первое число: ");
-            calc.setA(scan.nextInt());
-            System.out.println("Введите знак математической операции: ");
-            calc.setSign(scan.next().charAt(0));
-            System.out.println("Введите второе число: ");
-            calc.setB(scan.nextInt());
+            if (choice.equals("yes")) {
+                System.out.print("\nВведите математическое выражение: ");
+                String input = scan.nextLine().trim();
+                String[] expression = input.split(" ");
+                int firstNum = Integer.parseInt(expression[0]);
+                int secondNum = Integer.parseInt(expression[2]);
+                String sign = expression[1];
+                double result = Calculator.calculate(firstNum, sign, secondNum);
+                System.out.print(firstNum + " " + sign + " " + secondNum + " = ");
+                printResult(result);
+                scan.nextLine();
+            }
+            System.out.println("\nХотите продолжить вычисления? [yes / no]: ");
+            choice = scan.nextLine().toLowerCase();
+            if (!choice.equals("yes") && !choice.equals("no")) {
+                System.out.println("Введите корректный ответ [yes / no]:");
+            }
+        } while (!choice.equals("no"));
+    }
 
-            System.out.println("Результат равен: " + calc.calculate());
-            scan.nextLine();
-
-            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]: ");
-                choice = scan.nextLine();
-            } while (!choice.equals("no") && !choice.equals("yes"));
-        } while (choice.equals("yes"));
+    public static void printResult(double result) {
+        String strPattern = "#####.###";
+        DecimalFormat dFormat = new DecimalFormat(strPattern);
+        String formattedResult = dFormat.format(result);
+        System.out.print(formattedResult);
     }
 }
